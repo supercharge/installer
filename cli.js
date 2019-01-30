d86c42c6346f3fb33a0ffa392736a66f862aeadc
Marcus Pöhls <marcus.poehls@gmail.com>
2019-01-30 11:02:25 +0100

    add update-notifier

--- cli.js
+++ cli.js
@@ -1,6 +1,11 @@
+#!/usr/bin/env node
 'use strict'
 
 const Ace = require('@adonisjs/ace')
+const Pkg = require('./package.json')
+const UpdateNotifier = require('update-notifier')
+
+UpdateNotifier({ pkg: Pkg }).notify({ isGlobal: true })
 
 Ace.addCommand(require('./commands/new'))
 Ace.wireUpWithCommander()
