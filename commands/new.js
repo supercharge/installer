eeea615b36460c36a4dc27585eefdb1411d5102d
Marcus Poehls <marcus.poehls@gmail.com>
2019-08-19 07:09:06 +0200

    add error handling; reset version on setup

--- commands/new.js
+++ commands/new.js
@@ -6,6 +6,8 @@ const Listr = require('listr')
 const Execa = require('execa')
 const { promisify } = require('util')
 const readDir = promisify(Fs.readdir)
+const readFile = promisify(Fs.readFile)
+const writeFile = promisify(Fs.writeFile)
 const { Command } = require('@adonisjs/ace')
 
 class New extends Command {
@@ -26,35 +28,53 @@ class New extends Command {
     return 'Create a new Supercharge application'
   }
 
+  /**
+   * Create a new, greenfield Supercharge project.
+   *
+   * @param {Object} parameters
+   */
   async handle ({ name }) {
-    this.appPath = Path.resolve(process.cwd(), name)
+    try {
+      this.appPath = Path.resolve(process.cwd(), name)
 
-    this.success('Crafting a new Supercharge application\n')
+      this.success('Crafting a new Supercharge application\n')
 
-    const tasks = new Listr([
-      {
-        title: 'Ensure installation directory is empty',
-        task: () => this.ensureEmptyInstallPath(name)
-      },
-      {
-        title: 'Crafting your application',
-        task: () => this.cloneApp()
-      },
-      {
-        title: 'Install application dependencies',
-        task: () => this.installAppDependencies()
-      },
-      {
-        title: 'Initialize application setup',
-        task: () => Promise.resolve()
-      }
-    ])
+      const tasks = new Listr([
+        {
+          title: 'Ensure installation directory is empty',
+          task: () => this.ensureEmptyInstallPath(name)
+        },
+        {
+          title: 'Crafting your application',
+          task: () => this.craftApp()
+        },
+        {
+          title: 'Install application dependencies',
+          task: () => this.installAppDependencies()
+        },
+        {
+          title: 'Initialize application setup',
+          task: () => Promise.resolve()
+        }
+      ])
 
-    await tasks.run()
-    await this.runAppSetup(name)
-    this.success('\nEnjoy the ride!')
+      await tasks.run()
+      await this.runAppSetup(name)
+
+      this.success('\nEnjoy the ride!')
+    } catch (error) {
+      this.prettyPrintError(error)
+      process.exit(1)
+    }
   }
 
+  /**
+   * Ensure the installation directory doesn’t exist yet.
+   *
+   * @param {String} appName
+   *
+   * @throws
+   */
   async ensureEmptyInstallPath (appName) {
     if (!await this.pathExists(this.appPath)) {
       return
@@ -67,21 +87,61 @@ class New extends Command {
     }
   }
 
-  async cloneApp () {
+  /**
+   * Clone the blueprint to local disk
+   * and remove the `.git` folder.
+   */
+  async craftApp () {
     await Execa('git', ['clone', '--depth=1', this.blueprint, this.appPath])
     await this.removeDir(Path.resolve(this.appPath, '.git'))
+    await this.resetVersion()
   }
 
+  /**
+   * Reset the default Supercharge applicaiton
+   * and set it to `0.0.0`.
+   */
+  async resetVersion () {
+    const pkgPath = Path.resolve(this.appPath, 'package.json')
+
+    const pkg = JSON.parse(
+      await readFile(pkgPath, 'utf8')
+    )
+
+    pkg.version = '0.0.0'
+
+    await writeFile(pkgPath, JSON.stringify(pkg, null, 2))
+  }
+
+  /**
+   * Run `npm install`.
+   */
   async installAppDependencies () {
     return Execa('npm', ['install'], { cwd: this.appPath })
   }
 
+  /**
+   * Run the `node craft setup` command to
+   * create a `.env` file and generate
+   * an application key.
+   *
+   * @param {String} name
+   */
   async runAppSetup (name) {
     return Execa('node', ['craft', 'setup', `--name=${name}`], {
       stdin: 'inherit',
       stdout: 'inherit',
       cwd: this.appPath
     })
+  }
+
+  /**
+   * Pretty print the error message.
+   *
+   * @param {Object} error
+   */
+  prettyPrintError (error) {
+    console.error(`\n  ${this.chalk.bgRed.white(' Error ')} ${this.chalk.red(error.message)}\n`)
   }
 }
 
